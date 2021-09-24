4782dfb7bb2443744057a15fc16bb91ac1eac9fe
JairPPereira <jairppereira21@gmail.com>
2021-09-24 03:19:03 -0300

    ok

---
+++ root/js/default.js
@@ -0,0 +1,12 @@
+$(function() {
+    // Definir playlist
+    var playlist = [{
+        artist: 'Tony Brasil',
+        title: 'Amor não é prisão',
+        mp3: 'songs/amornaoeprisao.mp3'
+    }, {
+        artist: 'Tony Brasil',
+        title: 'Choro por voce',
+        mp3:'songs/choroporvoce.mp3'
+    }];
+ });
