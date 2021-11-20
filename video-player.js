98107cd6e8add97bad53e187a16083df7095a675
JairPPereira <jairppereira21@gmail.com>
2021-11-20 17:48:17 -0300

    iu

---
+++ video-player.js
@@ -0,0 +1,14 @@
+var player = videojs('videoPlayer', {
+    autoplay: 'muted',
+    controls: true,
+    poster:'',
+    loop: true,
+   
+    playbackRates:[0.25, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+    plugins: {
+        hotkeys: {
+
+        }
+    }
+   
+});
